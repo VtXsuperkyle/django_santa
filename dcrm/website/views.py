c522e315a18996b112ff5ca2941c7ee79a73f543
Kyle Sables <160232706@student.rnngroup.ac.uk>
2024-12-13 14:35:55 +0000

    sigmas are sigma

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
 from .forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'website/index.html')
@@ -17,3 +20,34 @@ def register(request):
     context = {'form': form}
 
     return render(request, 'website/register.html', context=context)
+
+#login a user
+def my_login(request):
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request,data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
+
+    context = {'login_form':form}
+    return render(request,'website/my-login.html', context=context)
+
+#logout user
+def user_logout(request):
+
+    auth.logout(request)
+    return redirect("my-login")
+
+#dashboard
+@login_required(login_url='my-login')
+def dashboard(request):
+    return render(request, 'website/dashboard.html')
