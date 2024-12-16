fed1c1ca776ff562d0120e6ecf84dc1459c8b955
VtXsuperkyle <160232706@student.rnngroup.ac.uk>
2024-12-16 16:17:25 +0000

    just copy the zoo work

--- dcrm/website/forms.py
+++ dcrm/website/forms.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
+from .models import santa_user
 from django import forms
 from django.forms.widgets import PasswordInput, TextInput
+
+
+
 
 #register or create a user
 class CreateUserForm(UserCreationForm):
 
     class Meta:
-        model = User
+        model = santa_user
         fields = ['username','password1','password2']
 
 
@@ -16,3 +19,32 @@ class CreateUserForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
+
+#add record
+class CreationRecordForm(forms.ModelForm):
+    class Meta:
+        model = santa_user
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city']
+
+class UpdateRecordForm(forms.ModelForm):
+        class Meta:
+             model = santa_user
+             fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city']
+class reindeer_booking(forms.ModelForm):
+     
+    class Meta:
+        model = reindeerbooking
+
+        fields = ['ride_booking_date_arrive', 'ride_booking_date_leave', 'ride_booking_adults',
+        'ride_booking_children', 'ride_total_cost']
+        labels={
+             "ride_booking_date_arrive": 'day you wish to arrive'
+        }
+        widgets = {
+             'ride_booking_date_arrive': forms.DateInput(attrs={'type': 'date'}),
+             'ride_booking_date_leave': forms.DateInput(attrs={'type': 'date'}),
+             'ride_total_cost': forms.HiddenInput(),
+        }
+
+    def __init__(self, *args, **kwargs):
+         super().__init__(*args,**kwargs)
--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class santa_user(AbstractUser):
+    points = models.IntegerField(default=0)
+    address = models.CharField(max_length=200, blank=True)
+    phone = models.CharField(max_length=14, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+
+class reindeerbooking(models.Model):
+    booking_id = models.AutoField(primary_key=True, editable=False)
+    ride_user_id = models.AutoField(santa_user, on_delete=models.CASCADE)
+    ride_booking_date = models.DateField(auto_now_add=True)
+    ride_booking_date_arrive = models.DateField()
+    ride_booking_date_leave = models.DateField()
+    ride_booking_adults = models.IntegerField(default=0)
+    ride_booking_children = models.IntegerField(default=0)
+    ride_total_cost = models.FloatField(default=0)
+
